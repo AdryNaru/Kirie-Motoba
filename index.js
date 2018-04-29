64361297d7630a48b2653032d8f5d7afa74c31cb
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 17:03:31 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const token = process.env.TOKEN;
+const config = require("./config.josn");
+const fs = require("fs");
+const token = process.env.TOKEN
 
 //pour le prefix//
 let prefix = "k!"
@@ -8,18 +10,28 @@ let prefix = "k!"
 
 client.on("message", (message) => {
         //vérification du prefix//
-        if (!message.content.startsWith(prefix) || message.author.bot) return;
+        if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
         //commands//
-        if (message.content.startsWith(prefix + "ping")) {
+        //ping//
+        if (message.content.startsWith(config.prefix + "ping")) {
             message.channel.send("pong connard tu te crois drole");
         } else
-
-        if (message.content.startsWith(prefix + "kirie")) {
+        //kirie//
+        if (message.content.startsWith(config.prefix + "kirie")) {
             message.channel.send("ne ne ne me parle pas ");
         }
+        //prefix//
+        if (message.content.startsWith(config.prefix + "prefix")) {
+            let newPrefix = message.content.split(" ").slice(1, 2)[0];
+            // change le prefix//
+            config.prefix = newPrefix;
+            //save du fichier config//
+            fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error)
+        }
+    
 });
 
 
 
-client.login(token)
+client.login(token);
