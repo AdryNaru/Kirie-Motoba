65a968d1c69e6b9620b699f19538bdbe93f61fc6
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:27:05 +0200

    Update test.js

--- commands/Umaru/test.js
+++ commands/Umaru/test.js
@@ -4,7 +4,6 @@ function test(message,prefix,client){
             .setColor(0x00ffff)
             .setDescription("Il faut que j'aille m'occuper de Umaru-chan désoler")
             .setImage("https://78.media.tumblr.com/3b2f19dbf1e7b9c6ffaecfc7388a6fb1/tumblr_oy914zzmQs1tm1dgio6_r1_500.gif")
-            .addBlankField(true)
           
             message.channel.send({embedtest}); 
     
