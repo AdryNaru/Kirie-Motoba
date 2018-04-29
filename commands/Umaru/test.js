f4be627d98801cc0f97498ab1734b1fc35d7fff3
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:25:02 +0200

    Update test.js

--- commands/Umaru/test.js
+++ commands/Umaru/test.js
@@ -1,12 +1,12 @@
 function test(message,prefix,client){
   if (message.content.startsWith(prefix + "test")) {
-                const embed = new Discord.RichEmbed()
+                const embedtest = new Discord.RichEmbed()
             .setColor(0x00ffff)
             .setDescription("Il faut que j'aille m'occuper de Umaru-chan désoler")
             .setImage("https://78.media.tumblr.com/3b2f19dbf1e7b9c6ffaecfc7388a6fb1/tumblr_oy914zzmQs1tm1dgio6_r1_500.gif")
             .addBlankField(true)
           
-            message.channel.send({embed}); 
+            message.channel.send({embedtest}); 
     
     }
 }
