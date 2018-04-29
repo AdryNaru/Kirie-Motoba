b859a0691990c63e649a69fae0feec3c0fb566ef
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 14:15:37 +0200

    Add files via upload

---
+++ commands/Umaru/say.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client ();
+const args = message.content.slice(config.prefix.lenght).trim().split(/ +/g);
+const command = agrs.shift().toLowerCase();
+
+function test(message,prefix,client){
+  if (message.content.startsWith(prefix + "say")) {
+    const sayMessage = args.join(" ");
+    message.delete.catch(O_o=>{});
+    message.channel.send(sayMessage);
+    
+    }
+}
+    module.exports = say
