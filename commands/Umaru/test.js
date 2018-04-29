9b899c2bb3ea56330a667fff3a1e9244810c9509
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:28:25 +0200

    Update test.js

--- commands/Umaru/test.js
+++ commands/Umaru/test.js
@@ -1,3 +1,6 @@
+const Discord = require('discord.js');
+const client = new Discord.Client ();
+
 function test(message,prefix,client){
   if (message.content.startsWith(prefix + "test")) {
                 const embedtest = new Discord.RichEmbed()
