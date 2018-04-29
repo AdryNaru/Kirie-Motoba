1b1f933d5f660cd1cb6cfb771bd0897211fc946c
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:01:24 +0200

    Update help.js

--- commands/Umaru/help.js
+++ commands/Umaru/help.js
@@ -6,15 +6,15 @@ const client = new Discord.Client ();
                 if (message.content.startsWith(prefix + "help")) {
             const embed = new Discord.RichEmbed()
             .setTitle("**COMMANDES**")
-            .setAuthor("Umaru-chan", "http://st.cdjapan.co.jp/pictures/l/13/10/THCS-60061.jpg")
-            .setColor(0xFF8000)
-            .setDescription("Voici la liste de mes commandes Oni-Chan")
-            .setFooter("Vive le cola et les chips! ONI-CHAAAAAAN va m'en acheté tout de suite", "https://image.dhgate.com/0x0/f2/albu/g1/M00/E8/60/rBVaGVZyej6AO5IGAAD4WO09TJQ211.jpg")
-            .setImage("http://www.volonte-d.com/temp/ange/umaruchan/umaruchanC.gif")
-            .setThumbnail("http://thehypedgeek.com/wp-content/uploads/2015/03/UmaruChan.jpg")
+            .setAuthor("Kirie", "https://myanimelist.cdn-dena.com/images/characters/12/287024.jpg")
+            .setColor(#87CEFF)
+            .setDescription("voila mes mes mes commande master")
+            .setFooter("je je je je je je humaru-chaaaaan", "https://thumbs.gfycat.com/FlickeringAfraidIcelandichorse-mobile.jpg")
+            .setImage("https://pa1.narvii.com/5959/525c77e26b72512b02dfffa25bf0d7602dd04759_hq.gif")
+            .setThumbnail("https://pa1.narvii.com/6028/55e6fb94f2512b2e600d02dc14ac413861c96de8_hq.gif")
             .setTimestamp()
-            .setURL("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMuui9P39guDu-WBmqpNd8DwnNloZ71REK2vOThm7b7dEEu-JFmQ")
-            .addField(":computer: **UMARU**",
+            .setURL("https://pa1.narvii.com/5869/855517d925623a54a04ee47faf60eab436fbbace_hq.gif")
+            .addField(":computer: **Kirie**",
               "``help``, ``invite``")
             .addField(":notes: **Musique**", "``disconnect``, ``nowplaying``, ``pause``, ``play``, ``playlist``, ``resume``, ``skip``, ``stop``, ``summon``", true)
             .addBlankField(true)
