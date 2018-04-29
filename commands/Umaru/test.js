2d3c3b8b2f3f32c314e334c27cdafdccd743e134
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-29 09:17:52 +0200

    Update test.js

--- commands/Umaru/test.js
+++ commands/Umaru/test.js
@@ -1,6 +1,6 @@
 function test(message,prefix,client){
   if (message.content.startsWith(prefix + "test")) {
-     message.channel.send("J'ai assez travaillé Oni-chan laisse moi jouer https://giphy.com/gifs/christmas-meme-uNf5zeBaofZNC");
+     message.channel.send("Il faut que j'aille m'occuper de Umaru-chan désoler https://78.media.tumblr.com/3b2f19dbf1e7b9c6ffaecfc7388a6fb1/tumblr_oy914zzmQs1tm1dgio6_r1_500.gif");
     }
 }
     module.exports = test
