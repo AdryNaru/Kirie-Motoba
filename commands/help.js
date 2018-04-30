25fa70e35503f9a1fb2eb480d383935337cc3f47
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-30 17:48:37 +0200

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -8,13 +8,13 @@ exports.run = (client, message, args) => {
     .setDescription("voila mes mes mes commande master")
     .setFooter("je je je je je je humaru-chaaaaan", "https://thumbs.gfycat.com/FlickeringAfraidIcelandichorse-mobile.jpg")
     .setImage("https://pa1.narvii.com/5869/855517d925623a54a04ee47faf60eab436fbbace_hq.gif")
-    .setThumbnail("https://pa1.narvii.com/6028/55e6fb94f2512b2e600d02dc14ac413861c96de8_hq.gif")
+    .setThumbnail("https://78.media.tumblr.com/6727ea99541e535838c33d6ed1daac6b/tumblr_p03ea4ANTy1tx45yjo1_500.gif")
     .setTimestamp()
     .setURL("https://pa1.narvii.com/5959/525c77e26b72512b02dfffa25bf0d7602dd04759_hq.gif ")
     .addField(":computer: **Kirie**",
       "``help``, ``kirie``, ``say``, ``ping``", true)
     .addBlankField(true)
-  
+    message.delete();
     message.channel.send({embed}).catch(console.error);
     console.log
 }
