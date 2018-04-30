ae13959d4c6639b7030c98f3a114a0f0fb94e00b
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-30 17:04:37 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -21,11 +21,7 @@ fs.readdir("./commands/", (err, files) => {
         console.log
         let eventFunction = require(`./commands/${file}`);
         console.log
-        var eventName = file.split(".")[0];
-        console.log
-        var sousdossier = eventName.split("/")
-        console.log
-        let eventName = sousdossier[sousdossier.length-1];
+        let eventName = file.split(".")[0];
         console.log
         client.on(eventName, (...args) => eventFunction.run(client, ...args));
         console.log
