08f5660832963270331f7a5039a77a293d073e1a
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-04-30 18:18:32 +0200

    Add files via upload

---
+++ commands/prefix.js
@@ -0,0 +1,11 @@
+
+exports.run = (client, message, args) => {
+    const fs = require("fs");
+    let newPrefix = message.content.split(" ").slice(1, 2)[0];
+            // change le prefix//
+            config.prefix = newPrefix;
+            message.channel.send("le prefix est changer")
+            //save du fichier config//
+            fs.writeFile("./config.json", JSON.stringify(config), (err) => console.error).catch(console.error);
+    console.log
+}
