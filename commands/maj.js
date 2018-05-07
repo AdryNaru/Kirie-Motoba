a6dc2f00cac089b1dd39ffc38dcd147ea588ab91
AdryNaru <38648912+AdryNaru@users.noreply.github.com>
2018-05-07 13:37:39 +0200

    Add files via upload

---
+++ commands/maj.js
@@ -0,0 +1,10 @@
+exports.run = (client, message, args) => {
+    const Discord = require("discord.js");
+    const embed = new Discord.RichEmbed()
+    .setColor(0x00FFFF)
+    .setDescription("On n'a enfin une maj de faite")
+    .setImage("https://media.giphy.com/media/fGR0AL2DhNODnOgEUG/giphy.gif")
+    message.delete();
+    message.channel.send({embed}).catch(console.error);
+    console.log
+}
